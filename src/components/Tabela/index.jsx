97b3f2f6696158d429c4687d1ce167d5c4f8bf2f
Emanuel Marcos <105237236+ebarbozadev@users.noreply.github.com>
2023-05-24 20:09:28 -0300

    Testando tabela

--- src/components/Tabela/index.jsx
+++ src/components/Tabela/index.jsx
@@ -2,33 +2,42 @@ import React from 'react';
 import styles from './Tabela.module.css';
 
 export default function Tabela({ usuarios }) {
-    return (
-        <table className={styles.tabela}>
-            <caption>Usuários cadastrados</caption>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>NAME</th>
-                    <th>EMAIL</th>
-                    <th>CELULAR</th>
-                    <th>WEBSITE</th>
-                    <th>OPÇÕES</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    usuarios.map((usuario) => (
-                        <tr>
-                            <td>{usuario.id}</td>
-                            <td>{usuario.name}</td>
-                            <td>{usuario.email}</td>
-                            <td>{usuario.phone}</td>
-                            <td>{usuario.website}</td>
-                            <td>Alterar Excluir</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
-    );
+  const handleAlterar = (id) => {
+    // Lógica para alterar usuário com o ID especificado
+  };
+
+  const handleExcluir = (id) => {
+    // Lógica para excluir usuário com o ID especificado
+  };
+
+  return (
+    <table className={styles.tabela}>
+      <caption>Usuários cadastrados</caption>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+          <th>EMAIL</th>
+          <th>CELULAR</th>
+          <th>WEBSITE</th>
+          <th>OPÇÕES</th>
+        </tr>
+      </thead>
+      <tbody>
+        {usuarios.map((usuario) => (
+          <tr key={usuario.id}>
+            <td>{usuario.id}</td>
+            <td>{usuario.name}</td>
+            <td>{usuario.email}</td>
+            <td>{usuario.phone}</td>
+            <td>{usuario.website}</td>
+            <td>
+              <button onClick={() => handleAlterar(usuario.id)}>Alterar</button>
+              <button onClick={() => handleExcluir(usuario.id)}>Excluir</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,42 +1,28 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Home.module.css';
 import Tabela from '../components/Tabela';
 import pegarURL from '../axios/config';
 
 export default function Home() {
-    const [usuarios, setUsuarios] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
 
-    // Uma função asyncrona que vai resgatar os dados dos adicionados
-    const getAdicionados = async () => {
-        // Por ser uma função asyncrona ela pode falhar, por isso vamos colocar em um try catch
-        try {
-            // Armazena a resposta do get do axios que pega as informações da API
-            const resposta = await pegarURL.get("/users");
-
-            // Como nós vimos no console.log(resposta) ele nos retorna um objeto e tem um "data" com as informações, queremos só ela então:
-            const data = resposta.data;
-
-            // Vamos setar os uduários com a nossa variável
-            setUsuarios(data);
-            console.log(usuarios)
-        } catch (erro) {
-            console.log(erro)
-        }
+  const getAdicionados = async () => {
+    try {
+      const resposta = await pegarURL.get('/users');
+      const data = resposta.data;
+      setUsuarios(data);
+    } catch (erro) {
+      console.log(erro);
     }
+  };
 
-    // Controla a execução da função quantas vezes quiser
-    // O array serve para executar apenas uma vez
-    useEffect(() => {
-        // Vamos executar e mostrar os adicionados
-        getAdicionados()
-    }, [])
+  useEffect(() => {
+    getAdicionados();
+  }, []);
 
-    return (
-        <div className='container'>
-            {
-                (usuarios.length === 0) ? <p>Não há usuários cadastrados</p>
-                    : <Tabela usuarios={usuarios} />
-            }
-        </div>
-    );
+  return (
+    <div className="container">
+      {usuarios.length === 0 ? <p>Não há usuários cadastrados</p> : <Tabela usuarios={usuarios} />}
+    </div>
+  );
 }
