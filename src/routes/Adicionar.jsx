f00234572136c3180f530d9ad77ca32e1aa68bed
Emanuel Marcos <105237236+ebarbozadev@users.noreply.github.com>
2023-05-24 19:50:55 -0300

    Atualizando formulário

--- src/routes/Adicionar.jsx
+++ src/routes/Adicionar.jsx
@@ -70,22 +70,22 @@ export default function Adicionar() {
                     placeholder="Digite o website"
                 /> */}
 
-                <div>
+                <div className={styles.campo__adicionar}>
                     <label htmlFor="nome">Nome</label>
                     <input type="text" id='nome' placeholder='Digite o conteúdo' onChange={(evento) => setNome(evento.target.value)} />
                 </div>
 
-                <div>
+                <div className={styles.campo__adicionar}>
                     <label htmlFor="email">Email</label>
                     <input type="email" id='email' placeholder='Digite o conteúdo' onChange={(evento) => setEmail(evento.target.value)} />
                 </div>
 
-                <div>
+                <div className={styles.campo__adicionar}>
                     <label htmlFor="celular">Celular</label>
                     <input type="text" id='celular' placeholder='Digite o conteúdo' onChange={(evento) => setCelular(evento.target.value)} />
                 </div>
 
-                <div>
+                <div className={styles.campo__adicionar}>
                     <label htmlFor="website">Website</label>
                     <input type="text" id='website' placeholder='Digite o conteúdo' onChange={(evento) => setWebsite(evento.target.value)} />
                 </div>
